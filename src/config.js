95d10f0e21d5e383805e148c6dd574c4b90d31b5
Antoine Maes <antoinemaes@gmail.com>
2020-11-05 11:40:15 +0100

    fix aws config

--- src/config.js
+++ src/config.js
@@ -10,7 +10,7 @@ export default {
     cognito: {
         region: "eu-west-2",
         userPoolId: "eu-west-2_DVxSROkiV",
-        userPoolWebClientOd: "7rfulsb6a4ebk45lv6jhtun8un",
+        userPoolWebClientId: "7rfulsb6a4ebk45lv6jhtun8un",
         identityPoolId: "eu-west-2:40484364-f1af-48cf-a615-14f921ce7ca0"
     }
 };
