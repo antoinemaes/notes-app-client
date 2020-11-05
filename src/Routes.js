f3dc277833bdcf854f959e51665c2e27b1f37134
Antoine Maes <antoinemaes@gmail.com>
2020-11-05 09:07:46 +0100

    first components

---
+++ src/Routes.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./containers/Home";
+
+export default () => (
+    <Switch>
+        <Route exact path="/">
+            <Home />
+        </Route>
+    </Switch>
+);
---
+++ src/containers/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Home.css";
+
+export default () => (
+    <div className="Home">
+        <div className="lander">
+            <h1>Scratch</h1>
+            <p>A simple note taking app</p>
+        </div>
+    </div>
+);
